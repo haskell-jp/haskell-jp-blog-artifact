178c29258dd14a78c95848c77580fb0d07797505
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-02-24 16:45:37 +0900

    fix indentation

--- app/Main.hs
+++ app/Main.hs
@@ -13,13 +13,12 @@ import Network.HTTP.Client.TLS
 import qualified Data.Text as T
 import Data.Aeson as J
 import Data.Aeson.Types as J
-import Data.Monoid
 import System.Environment
 
 sendResponse :: Monad m => (Response -> m b) -> ExceptT BL.ByteString m b -> m b
 sendResponse send m = runExceptT m >>= \case
-    Left e -> send $ responseLBS status400 [] e
-    Right a -> return a
+  Left e -> send $ responseLBS status400 [] e
+  Right a -> return a
 
 commentArtifact :: Object -> Parser (IO HC.Request)
 commentArtifact obj = do
@@ -33,18 +32,18 @@ commentArtifact obj = do
     initialRequest <- HC.parseRequest
       $ T.unpack $ T.intercalate "/" ["https://api.github.com/repos", owner, repo, "commits", sha, "comments"]
     return initialRequest
-          { HC.method = "POST"
-          , HC.requestBody = HC.RequestBodyLBS $ J.encode $ object
-            [ "body" .= T.intercalate "/"
-              [ buildUrl, "artifacts", "0" <> home, "project"
-              , "generated-site/index.html"]
-            -- TODO: , "path" .=
-            ]
-          , HC.requestHeaders =
-            [ ("Authorization", B.pack token)
-            , ("User-Agent", "haskell-jp-blog-artifact")
-            ]
-          }
+      { HC.method = "POST"
+      , HC.requestBody = HC.RequestBodyLBS $ J.encode $ object
+        [ "body" .= T.intercalate "/"
+          [ buildUrl, "artifacts", "0" <> home, "project"
+          , "generated-site/index.html"]
+        -- TODO: , "path" .=
+        ]
+      , HC.requestHeaders =
+        [ ("Authorization", B.pack token)
+        , ("User-Agent", "haskell-jp-blog-artifact")
+        ]
+      }
 
 main :: IO ()
 main = do
